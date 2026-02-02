02a06a3070e1fee1028cc9594d7e88aaa57f6981
Jasmin251 <jschmirler04@gmail.com>
2026-02-02 17:40:02 +0100

    integrated database

--- Zoorganize/Form1.cs
+++ Zoorganize/Form1.cs
@@ -1,10 +1,32 @@
+using Microsoft.Data.Sqlite;
+using System.Data;
+
 namespace Zoorganize
 {
     public partial class Form1 : Form
     {
+        private string connectionString = "Data Source=Database/Zoorganize.db";
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using SqliteConnection conn =
+                new SqliteConnection(connectionString);
+            conn.Open();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText =
+                "SELECT Id FROM Pfleger LIMIT 1";
+
+            object result = cmd.ExecuteScalar();
+
+            if (result != null)
+                MessageBox.Show(result.ToString());
+            else
+                MessageBox.Show("Keine Daten in der Tabelle.");
         }
     }
 }
